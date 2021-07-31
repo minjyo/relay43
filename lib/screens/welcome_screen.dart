436a6cf3fd4e87669dabc56a5f690fd64661ea91
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-07-31 15:40:13 +0900

    Revert "Merge pull request #14 from boostcampwm-2021/jeonghan_UI"
    This reverts commit 4f2d172a32c5dbfb1744e53c29381099a53eb31c, reversing
    changes made to 2d45f372d9dc1d7610f41ad1e1b0ac3d6b0519f6.

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -19,123 +19,36 @@ class _WelcomeScreenState extends State<WelcomeScreen>
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Padding(
-              padding: const EdgeInsets.fromLTRB(0, 150, 0, 0),
+              padding: const EdgeInsets.symmetric(vertical: 40.0),
               child: SizedBox(
-                height: 100,
-                width: 350,
-                child: Text(
-                  "ZZom",
-                  style: TextStyle(
-                      fontFamily: "boorsok",
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 5.0,
-                      fontSize: 100,
-                      color: Colors.blue[400]),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              // child: Image.asset('images/logo.png')),
-            ),
-            SizedBox(
-              height: 300,
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(40, 0, 0, 80),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text(
-                      "INVITE",
-                      style: TextStyle(
-                        color: Colors.grey[300],
-                        fontWeight: FontWeight.bold,
-                        fontSize: 28,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Text(
-                      "YOUR",
-                      style: TextStyle(
-                        color: Colors.grey[300],
-                        fontWeight: FontWeight.bold,
-                        fontSize: 40,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Text(
-                      "TEAM",
-                      style: TextStyle(
-                        color: Colors.grey[300],
-                        fontWeight: FontWeight.bold,
-                        fontSize: 50,
-                      ),
-                    ),
-                  ],
-                ),
+                  height: 250,
+                  width: 250,
+                  child: Image.asset('images/logo.png')
               ),
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(vertical: 60.0),
+              padding: const EdgeInsets.symmetric(vertical: 30.0),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  SizedBox(
-                    width: 3000.0,
-                    height: 60.0,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, RegistrationScreen.id);
-                      },
-                      child: Text(
-                        "SIGN IN",
-                        style: TextStyle(
-                          // fontFamily: "boorsok",
-                          letterSpacing: 1.0,
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                          primary: Colors.blue[400],
-                          onPrimary: Colors.white,
-                          shape: new RoundedRectangleBorder(
-                            borderRadius: new BorderRadius.circular(30.0),
-                          )),
-                    ),
+                  MaterialButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, LoginScreen.id);
+                    },
+                    child: Text("Log In"),
+                    color: Colors.blue,
+                    textColor: Colors.white,
                   ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  SizedBox(
-                    width: 3000.0,
-                    height: 60.0,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, LoginScreen.id);
-                      },
-                      child: Text(
-                        "LOG IN",
-                        style: TextStyle(
-                          // fontFamily: "boorsok",
-                          letterSpacing: 1.0,
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                          primary: Colors.blue[400],
-                          onPrimary: Colors.white,
-                          shape: new RoundedRectangleBorder(
-                            borderRadius: new BorderRadius.circular(30.0),
-                          )),
-                    ),
-                  ),
+                  MaterialButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, RegistrationScreen.id);
+                    },
+                    child: Text("Register"),
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                  )
                 ],
               ),
             )

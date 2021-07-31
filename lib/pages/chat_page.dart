b079af13828e77910ec30497491420bdbfffc038
gidskql6671 <gidskql6671@gmail.com>
2021-07-31 23:18:57 +0900

    메시지가 오면 스크롤을 아래로 내림

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -18,12 +18,12 @@ class ChatPage extends StatefulWidget {
   _ChatPageState createState() => _ChatPageState();
 }
 
-class _ChatPageState extends State<ChatPage> {
+class _ChatPageState extends State<ChatPage> with WidgetsBindingObserver{
   Stream<QuerySnapshot>? _chats;
   TextEditingController messageEditingController = new TextEditingController();
   final ScrollController _scrollController = ScrollController();
   
-  Widget _chatMessages(Function callback){
+  Widget _chatMessages(){
     return StreamBuilder(
       stream: _chats,
       builder: (context, AsyncSnapshot<QuerySnapshot> snapshot){
@@ -32,19 +32,20 @@ class _ChatPageState extends State<ChatPage> {
           
           return Expanded(
             child: ListView.builder(
-                controller: _scrollController,
-                shrinkWrap: true,
-                scrollDirection: Axis.vertical,
-                itemCount: snapData!.docs.length,
-                itemBuilder: (context, index){
+              controller: _scrollController,
+              shrinkWrap: true,
+              scrollDirection: Axis.vertical,
+              itemCount: snapData!.docs.length,
+              itemBuilder: (context, index){
                 Map<String, dynamic> data = snapData.docs[index].data() as Map<String, dynamic>;
+                _scrollToBottom();
                 return MessageWidget(
                   data["message"] ,
                   data["sender"],
                   widget.userName == data["sender"],
                 );
               }
-        ),
+            ),
           );
         } else {
           return Container();
@@ -53,44 +54,65 @@ class _ChatPageState extends State<ChatPage> {
     );
   }
 
-  /*
-  메세지를 보내고 리스트를 받아오는 과정이 비동기 과정일 수도 있다는 생각이 들어
-  콜백으로 해보려고 만든 함수입니다.
-  해보니 제대로 먹히지도 않아서 필요에 따라 지우셔도 됩니다.
-  해당 콜백 함수는 _chatMessages(Function callback) 함수에서 사용합니다.
-  */
-  _scrollCallBack(){
-
-  }
-  _sendMessage() {
+  // 채팅 메시지를 메시지를 보낸다
+  _sendMessage({String type = "chat"}) {
     if (messageEditingController.text.isNotEmpty) {
       Map<String, dynamic> chatMessageMap = {
         "message": messageEditingController.text,
         "sender": widget.userName,
-        'time': DateTime.now().millisecondsSinceEpoch,
+        'time': FieldValue.serverTimestamp(),
+        "type": type
       };
 
       DatabaseService().sendMessage(widget.groupId!, chatMessageMap);
+      _scrollToBottom();
+      
       setState(() {
         messageEditingController.text = "";
-        Future.delayed(Duration(milliseconds: 100), () {
-          //500ms 동안 마지막 위치로 애니매이션 효과를 주면서 이동
-          _scrollController.animateTo(_scrollController.position.maxScrollExtent, duration: const Duration(milliseconds: 300), curve: Curves.ease);
-        },
-        );
       });
     }
   }
 
+  _scrollToBottom(){
+    Future.delayed(Duration(milliseconds: 100), () {
+      //500ms 동안 마지막 위치로 애니매이션 효과를 주면서 이동
+      _scrollController.animateTo(_scrollController.position.maxScrollExtent, duration: const Duration(milliseconds: 300), curve: Curves.ease);
+      },
+    );
+  }
+  
   @override
   void initState() {
     super.initState();
+    WidgetsBinding.instance?.addObserver(this);
+    
+    DatabaseService().joinGroup(widget.groupId!, widget.userName!);
     setState(() {
       this._chats = DatabaseService().getChats(widget.groupId!);
 
     });
   }
+  
+  @override
+  void dispose(){
+    super.dispose();
+    WidgetsBinding.instance?.removeObserver(this);
+  }
 
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    switch(state){
+      case AppLifecycleState.paused:
+      case AppLifecycleState.detached:
+        DatabaseService().exitGroup(widget.groupId!, widget.userName!);
+        Navigator.pop(context);
+        break;
+      default:
+    }
+  }
+  
+  // 뒤로가기 로직 처리
   _onBackPressed(BuildContext context){
     return showDialog( 
         context: context, 
@@ -120,6 +142,8 @@ class _ChatPageState extends State<ChatPage> {
 
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance?.addPostFrameCallback((_) => _scrollToBottom());
+    
     return WillPopScope(
       onWillPop: () => _onBackPressed(context),
       child:
@@ -133,7 +157,7 @@ class _ChatPageState extends State<ChatPage> {
         body: Container(
           child: Column(
             children: <Widget>[
-              _chatMessages(_scrollCallBack),
+              _chatMessages(),
               Divider(
                 color: Colors.black,
                 thickness: 1,
@@ -168,7 +192,7 @@ class _ChatPageState extends State<ChatPage> {
   
                       GestureDetector(
                         onTap: () {
-                          _sendMessage();
+                          _sendMessage(type: "chat");
                         },
                         child: Container(
                           height: 50.0,
@@ -190,4 +214,5 @@ class _ChatPageState extends State<ChatPage> {
       ),
     );
   }
+
 }

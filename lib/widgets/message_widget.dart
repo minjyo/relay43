bffe5f2766b412bc0b1b033c72b917141c0e5a73
gidskql6671 <gidskql6671@gmail.com>
2021-08-01 22:30:05 +0900

    리팩토링 주석 추가, 불필요한 코드 제거, Indent 정렬

--- lib/widgets/message_widget.dart
+++ lib/widgets/message_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// 채팅방에서 사용하는 메시지 위젯
 class MessageWidget extends StatelessWidget {
 
   final String message;
@@ -17,23 +18,24 @@ class MessageWidget extends StatelessWidget {
           bottom: 4,
           left: sentByMe ? 0 : 24,
           right: sentByMe ? 24 : 0),
+      // 내가 보낸 것이면 오른쪽, 상대가 보낸 것이면 왼쪽에 배열
       alignment: sentByMe ? Alignment.centerRight : Alignment.centerLeft,
       child: Container(
         margin: sentByMe ? EdgeInsets.only(left: 30) : EdgeInsets.only(right: 30),
         padding: EdgeInsets.only(top: 17, bottom: 17, left: 20, right: 20),
         decoration: BoxDecoration(
-          borderRadius: sentByMe ? BorderRadius.only(
-              topLeft: Radius.circular(23),
-              topRight: Radius.circular(23),
-              bottomLeft: Radius.circular(23)
-          )
-              :
-          BorderRadius.only(
-              topLeft: Radius.circular(23),
-              topRight: Radius.circular(23),
-              bottomRight: Radius.circular(23)
-          ),
-          // color: sentByMe ? Colors.blueAccent : Colors.grey[700],
+          borderRadius: sentByMe ? 
+            BorderRadius.only(
+                topLeft: Radius.circular(23),
+                topRight: Radius.circular(23),
+                bottomLeft: Radius.circular(23)
+            )
+                :
+            BorderRadius.only(
+                topLeft: Radius.circular(23),
+                topRight: Radius.circular(23),
+                bottomRight: Radius.circular(23)
+            ),
           color: sentByMe ? Colors.blueAccent : Colors.blue,
         ),
         child: Column(
